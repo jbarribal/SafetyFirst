2c944a0ae2ef7df4f5f33e797dd5be19197a4e45
Joenard <romero.joenard@ue.edu.ph>
2021-05-08 12:31:22 +0800

    Creating Module Page

--- app/src/main/java/com/example/safetyfirst/DisasterPage.java
+++ app/src/main/java/com/example/safetyfirst/DisasterPage.java
@@ -1,5 +1,7 @@
 package com.example.safetyfirst;
 
+import android.content.Intent;
+import android.view.View;
 import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
@@ -18,6 +20,14 @@ public class DisasterPage extends AppCompatActivity {
         final Button btnEarthquake = (Button) findViewById(R.id.btnEarthquake);
         final Button btnFire       = (Button) findViewById(R.id.btnFire);
         final Button btnFlood      = (Button) findViewById(R.id.btnFlood);
+
+        btnEarthquake.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(DisasterPage.this, EarthquakeModule.class);
+                startActivity(intent);
+            }
+        });
 
 
     }
