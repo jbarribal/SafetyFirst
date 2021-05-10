5e727abe40d96d92e8a56b82f85187a5c579d4ff
[barribal.joshuageorge@ue.edu.ph] <barribal.joshuageorge@ue.edu.ph>
2021-05-10 16:39:52 +0800

    remove pretest.java and pretest.xml added questions class and quizactivity class

---
+++ app/src/main/java/com/example/safetyfirst/Questions.java
@@ -0,0 +1,52 @@
+package com.example.safetyfirst;
+
+public class Questions {
+
+
+
+
+    private String mQuestions [] = {
+
+
+    };
+
+
+    private String mChoices [][] = {
+
+    };
+
+
+
+    private String mCorrectAnswers[] = {};
+
+
+
+
+    public String getQuestion(int a) {
+        String question = mQuestions[a];
+        return question;
+    }
+
+
+    public String getChoice1(int a) {
+        String choice0 = mChoices[a][0];
+        return choice0;
+    }
+
+
+    public String getChoice2(int a) {
+        String choice1 = mChoices[a][1];
+        return choice1;
+    }
+
+    public String getChoice3(int a) {
+        String choice2 = mChoices[a][2];
+        return choice2;
+    }
+
+    public String getCorrectAnswer(int a) {
+        String answer = mCorrectAnswers[a];
+        return answer;
+    }
+
+}
